275bb5bfd5854ac1ca53bc5b665db47abf4b1d1f
Shekhar34 <shekhargite6123@gmail.com>
2024-02-28 16:04:31 +0530

    created the backend

--- express/index.js
+++ express/index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const fs=require('fs');
 var path = require('path');
 const app=express();
 
@@ -6,6 +7,9 @@ const port=5000;
 
 //serving static files express specific files
 app.use('/static', express.static('static'));
+
+//jeva form cha content backend la patvycha asto post karycha asto teva he use kara
+app.use(express.urlencoded())
 
 
 //set template engine pug pecific files
@@ -18,6 +22,17 @@ app.set('views',path.join(__dirname, 'views'));
 
 app.get('/',(req,res)=>{
     const param={'title':'pug is best game','content':'this is baby'}
+       res.status(200).render('index.pug',param);
+})
+
+app.post('/',(req,res)=>{
+    name=req.body.name;
+    email=req.body.email;
+    message=req.body.message;
+    let output=`the name of client is ${name} and email is ${email} and the msg is ${message}`;
+    fs.writeFileSync('output.text',output)
+
+    const param={'message':'form submitted sucessfully'}
        res.status(200).render('index.pug',param);
 })
 
